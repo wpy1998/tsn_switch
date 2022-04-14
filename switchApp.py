9aba562814a1a8719fa01b512a6d068681f9f2c8
wpy1998 <1251499395@qq.com>
2022-04-14 02:05:13 -0700

    commit by wpy

--- switchApp.py
+++ switchApp.py
@@ -1,10 +1,11 @@
+# -- coding:UTF-8 --
 import time
 import lldp
 import networktopologylauncher
 
 if __name__ == "__main__":
     cuc_ip = '10.2.25.4'
-    print('current cuc_ip： ', cuc_ip)
+    print('current cuc_ip: ', cuc_ip)
     print('you can change the cuc_ip in file: switchApp.py')
     lldpImpl = lldp.LLDPImpl()
     launcher = networktopologylauncher.NetworkTopologyLauncher(cuc_ip)
