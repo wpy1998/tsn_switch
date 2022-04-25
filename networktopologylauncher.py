e44bb5f96ea88da919b4f69ab80c77cd9fc32ce1
wpy1998 <1251499395@qq.com>
2022-04-24 20:47:16 -0700

    commit by wpy

--- networktopologylauncher.py
+++ networktopologylauncher.py
@@ -13,8 +13,25 @@ class NetworkTopologyLauncher:
     def registerSwitch(self, lldpImpl):
         url = self.urls.get("tsn-topology") + 'topology/tsn-network'
         topology = networkTopology.Topology('tsn-network', lldpImpl)
-        array = []
-        array.append(topology.get_json())
-        topologies = {}
-        topologies['topology'] = array
-        httpInfo.put_info(url, json.dumps(topologies))
+        # array = []
+        # array.append(topology.get_json())
+        # topologies = {}
+        # topologies['topology'] = array
+        # httpInfo.put_info(url, json.dumps(topologies))
+
+        nodes = topology.get_json().get('node')
+        for node in nodes:
+            array = []
+            target = {}
+            array.append(node)
+            target['node'] = array
+            print('--register node to controller--')
+            httpInfo.put_info(url + '/node/' + node['node-id'], json.dumps(target))
+        links = topology.get_json().get('link')
+        for link in links:
+            array = []
+            target = {}
+            array.append(link)
+            target['link'] = array
+            print('--register link to controller--')
+            httpInfo.put_info(url + '/link/' + link['link-id'], json.dumps(target))
