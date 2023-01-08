4ba16715ad5e17ca80d20357662c7f80e683b62f
wpy1998 <1251499395@qq.com>
2023-01-08 19:42:52 +0800

    commit by wpy

--- switchApp.py
+++ switchApp.py
@@ -3,7 +3,7 @@ import hardware.computer as hc
 import topology_launcher as tl
 
 if __name__ == "__main__":
-    cuc_ip = 'localhost'
+    cuc_ip = '192.168.0.119'
     print('*****************************************************************')
     print('current cuc_ip: ', hc.cuc_ip)
     print('you can change the cuc_ip in hardware.computer')
--- topology/timer_thread.py
+++ topology/timer_thread.py
@@ -26,14 +26,16 @@ class TimerThread(threading.Thread):
     def registerSwitch(self):
         url = self.url_front + 'topology/' + self.topology_id
         node = self.get_node_json()
-        nodes = self.topology.get_json().get('node')
-        for node in nodes:
-            array = []
-            target = {}
-            array.append(node)
-            target['node'] = array
-            print('<TSN switch> register node to controller <TSN switch>')
-            httpInfo.put_info(url + '/node/' + node['node-id'], json.dumps(target))
+        httpInfo.put_info(url + '/node/' + node['node-id'], json.dumps(node))
+        print('<TSN switch> register node to controller <TSN switch>')
+        # nodes = self.topology.get_json().get('node')
+        # for node in nodes:
+        #     array = []
+        #     target = {}
+        #     array.append(node)
+        #     target['node'] = array
+        #     print('<TSN switch> register node to controller <TSN switch>')
+        #     httpInfo.put_info(url + '/node/' + node['node-id'], json.dumps(target))
         # links = self.topology.get_json().get('link')
         # for link in links:
         #     array = []
@@ -45,10 +47,13 @@ class TimerThread(threading.Thread):
 
     def removeSwitch(self):
         url = self.url_front + 'topology/' + self.topology_id
-        nodes = self.topology.get_json().get('node')
-        for node in nodes:
-            print('<TSN switch> remove node from controller <TSN switch>')
-            httpInfo.delete_info(url + '/node/' + node['node-id'])
+        node = self.get_node_json()
+        httpInfo.delete_info(url + '/node/' + node['node-id'])
+        print('<TSN switch> remove node from controller <TSN switch>')
+        # nodes = self.topology.get_json().get('node')
+        # for node in nodes:
+        #     print('<TSN switch> remove node from controller <TSN switch>')
+        #     httpInfo.delete_info(url + '/node/' + node['node-id'])
         # links = self.topology.get_json().get('link')
         # for link in links:
         #     print('<TSN switch> remove link from controller <TSN switch>')
